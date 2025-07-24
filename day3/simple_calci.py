5fab644806080867cdb39b88154dabb6a7ecdbf1
raji024 <rajeshwariak7@gmail.com>
2025-07-24 19:19:31 +0530

    commit

---
+++ day3/simple_calci.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = int(input())
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print("addition of two numbers is",add)
+print("subtraction of two numbers is",sub)
+print("multiplication of two numbers is",mul);
+print("division of two number is",div);
