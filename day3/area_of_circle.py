d207f0466d60b4aed95d659e89c83229c44134a1
raji024 <rajeshwariak7@gmail.com>
2025-07-24 19:41:47 +0530

    commit

---
+++ day3/area_of_circle.py
@@ -0,0 +1,6 @@
+r = float(input())
+pi = 3.14
+area = pi * r * r
+peri = 2 * pi * r
+print(area)
+print(peri)
---
+++ day3/sqrt.py
@@ -0,0 +1,6 @@
+import math
+n = int(input())
+s_r = math.sqrt(n)
+s = n * n
+print(s_r)
+print(s)
