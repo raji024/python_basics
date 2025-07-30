9770b6f7e7c7825e42e61f53f713e15de772a9b6
raji024 <rajeshwariak7@gmail.com>
2025-07-30 11:31:11 +0530

    commit

---
+++ string/basic.py
@@ -0,0 +1,8 @@
+a = input()
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.title())
+print(a.split())
+print(a.replace("raji","raj"))
+
---
+++ string/slicing.py
@@ -0,0 +1,5 @@
+a ="simple"
+print(a[0])
+print(a[1:4])
+print(a[:-1])
+print(a[::-1])
